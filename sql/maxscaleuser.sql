303319b189f242a881fcbc5f2185378cfd757103
Frithjof <fbrate@outlook.com>
2020-05-06 13:30:51 +0200

    Update maxscaleuser.sql

--- sql/maxscaleuser.sql
+++ sql/maxscaleuser.sql
@@ -1,5 +1,5 @@
 grant select on mysql.* to 'maxscaleuser'@'maxscale' IDENTIFIED BY 'maxscalepass';
 grant replication slave on *.* to 'maxscaleuser'@'maxscale';
-grant replication client on *.* to 'maxscaleuser'@'maxscale;
+grant replication client on *.* to 'maxscaleuser'@'maxscale';
 grant show databases on *.* to 'maxscaleuser'@'maxscale;
 flush privileges;
