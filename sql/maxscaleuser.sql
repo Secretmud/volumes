98c52be712e27361145aa8dc67145c8c298094af
Frithjof <fbrate@outlook.com>
2020-05-06 13:28:13 +0200

    Update maxscaleuser.sql

--- sql/maxscaleuser.sql
+++ sql/maxscaleuser.sql
@@ -1,8 +1,5 @@
-create user 'maxscaleuser'@'maxscale' identified by 'maxscalepass';
-grant SELECT on mysql.user to 'maxscaleuser'@'maxscale';
-GRANT SELECT ON mysql.db TO 'maxscaleuser'@'maxscale';
-GRANT SELECT ON mysql.tables_priv TO 'maxscaleuser'@'maxscale';
-GRANT SHOW DATABASES ON *.* TO 'maxscaleuser'@'maxscale';
-grant REPLICATION SLAVE on *.* to 'maxscaleuser'@'maxscale';
-grant REPLICATION CLIENT on *.* to 'maxscaleuser'@'maxscale';
-
+grant select on mysql.* to 'maxscaleuser'@'maxscale' IDENTIFIED BY 'maxscalepass';
+grant replication slave on *.* to 'maxscaleuser'@'maxscale';
+grant replication client on *.* to 'maxscaleuser'@'maxscale;
+grant show databases on *.* to 'maxscaleuser'@'maxscale;
+flush privileges;
