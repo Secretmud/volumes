3ceaa397415cbebcb8635ea1922938c41d278bba
secretmud@gmail.com <lamefeed@gmail.com>
2020-05-12 12:30:11 +0200

    Fjernet dats42user.sql og sed-delen fra setup_all.sh

--- db1/init.db/dats42user.sql
+++
@@ -1,5 +0,0 @@
-grant select on mysql.* to 'MAX_USERNAME'@'MAXSCALE_IP' IDENTIFIED BY 'MAX_PASS';
-grant replication slave on *.* to 'MAX_USERNAME'@'MAXSCALE_IP';
-grant replication client on *.* to 'MAX_USERNAME'@'MAXSCALE_IP';
-grant show databases on *.* to 'MAX_USERNAME'@'MAXSCALE_IP';
-flush privileges;
--- setup_all.sh
+++ setup_all.sh
@@ -79,7 +79,6 @@ clean_volumes() {
     sudo echo -e  ~/volumes/web2/html/include/ ~/volumes/web3/html/include/ | sudo xargs -n 1 cp ~/volumes/web1/html/include/dbconnection.php
     sudo sed -i "s/MAX_USERNAME/$maxuser/g; s/MAX_PASS/$maxpass/g" ~/volumes/dbproxy/my-maxscale.cnf
     sudo sed -i "s/DATSUSER/$datsusername/g; s/MAX_USERNAME/$maxuser/g; s/MAX_PASS/$maxpass/g; s/MAXSCALE_IP/$databaseproxy_IP/g" ~/volumes/db1/init.db/maxscaleuser.sql
-    sudo sed -i "s/MAX_USERNAME/$maxuser/g; s/MAX_PASS/$maxpass/g; s/MAXSCALE_IP/$databaseproxy_IP/g" ~/volumes/db1/init.db/dats42user.sql
 
 }
 
